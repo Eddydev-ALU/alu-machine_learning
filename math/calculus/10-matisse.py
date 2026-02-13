a42ea3f7f064450a2de9e269d414b1874c3f8418
Eddydev-ALU <e.irasetsa@alustudent.com>
2026-02-13 01:35:30 -0800

    some change

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 """
-    a function def poly_derivative(poly):
-    that calculates the derivative of a polynomial
+a function def poly_derivative(poly):
+that calculates the derivative of a polynomial
 """
+
 
 def poly_derivative(poly):
     """
-        calculates the derivative of a polynomial
+    calculates the derivative of a polynomial
     """
 
-    # Validate poly
-    if (not isinstance(poly, list) or
-        len(poly) == 0 or
-        not all(isinstance(x, (int, float)) for x in poly)):
+    if not isinstance(poly, list) or len(poly) == 0:
         return None
 
-    # If polynomial is constant
+    if not all(isinstance(x, (int, float)) for x in poly):
+        return None
+
     if len(poly) == 1:
         return [0]
 
-    # Compute derivative
     derivative = [poly[i] * i for i in range(1, len(poly))]
-
     return derivative
